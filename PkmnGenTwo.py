f81040b29df1a6bd598c71e609557c7942b4faa1
Michael Shimmel <mjsiiisnumber1@gmail.com>
2024-05-19 21:44:50 -0400

    Generates a random number and prints the pokemon that correlates to its pokedex number

---
+++ PkmnGenTwo.py
@@ -0,0 +1,109 @@
+pkmn = range(0, 39)
+
+import random
+
+pkdex = random.choice(pkmn)
+
+if pkdex == 0:
+  random_pkmn = "Missingno"
+elif pkdex == 1:
+  random_pkmn = "Bulbasaur"
+elif pkdex == 2:
+  random_pkmn = "Ivysaur"
+elif pkdex == 3:
+  random_pkmn = "Venusaur"
+elif pkdex == 4:
+  random_pkmn = "Charmander"
+elif pkdex == 5:
+  random_pkmn = "Charmeleon"
+elif pkdex == 6:
+  random_pkmn = "Charizard"
+elif pkdex == 7:
+  random_pkmn = "Squirtle"
+elif pkdex == 8:
+  random_pkmn = "Wartortle"
+elif pkdex == 9:
+  random_pkmn = "Blastoise"
+elif pkdex == 10:
+  random_pkmn = "Caterpie"
+elif pkdex == 11:
+  random_pkmn = "Metapod"
+elif pkdex == 12:
+  random_pkmn = "Butterfree"
+elif pkdex == 13:
+  random_pkmn = "Weedle"
+elif pkdex == 14:
+  random_pkmn = "Kakuna"
+elif pkdex == 15:
+  random_pkmn = "Beedrill"
+elif pkdex == 16:
+  random_pkmn = "Pidgey"
+elif pkdex == 17:
+  random_pkmn = "Pidgeotto"
+elif pkdex == 18:
+  random_pkmn = "Pidgeot"
+elif pkdex == 19:
+  random_pkmn = "Rattata"
+elif pkdex == 20:
+  random_pkmn = "Raticate"
+elif pkdex == 21:
+  random_pkmn = "Spearow"
+elif pkdex == 22:
+  random_pkmn = "Fearow"
+elif pkdex == 23:
+  random_pkmn = "Ekans"
+elif pkdex == 24:
+  random_pkmn = "Arbok"
+elif pkdex == 25:
+  random_pkmn = "Pikachu"
+elif pkdex == 26:
+  random_pkmn = "Raichu"
+elif pkdex == 27:
+  random_pkmn = "Sandshrew"
+elif pkdex == 28:
+  random_pkmn = "Sandslash"
+elif pkdex == 29:
+  random_pkmn = "Nidoran♀"
+elif pkdex == 30:
+  random_pkmn = "Nidorina"
+elif pkdex == 31:
+  random_pkmn = "Nidoqueen"
+elif pkdex == 32:
+  random_pkmn = "Nidoran♂"
+elif pkdex == 33:
+  random_pkmn = "Nidorino"
+elif pkdex == 34:
+  random_pkmn = "Nidoking"
+elif pkdex == 35:
+  random_pkmn = "Clefairy"
+elif pkdex == 36:
+  random_pkmn = "Clefable"
+elif pkdex == 37:
+  random_pkmn = "Vulpix"
+elif pkdex == 38:
+  random_pkmn = "Ninetales"
+elif pkdex == 39:
+  random_pkmn = "Jigglypuff"
+
+
+print(random_pkmn)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
